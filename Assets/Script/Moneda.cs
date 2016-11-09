adf828092ec37b9a5397bd916aaa280ff0cc2530
Arbustus <jm.martinezroa@gmail.com>
2016-11-09 17:33:53 +0100

    Prefab moneda y organización de las carpetas
    Creacion de la carpeta prefab, la moneda y su script para que aparezca
    con una fuerza aleatoria hacia arriba en 180 grados.

---
+++ Assets/Script/Moneda.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moneda : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	void Start (){
+		Destroy (gameObject, 3);
+		rb = GetComponent<Rigidbody2D> ();
+		rb.AddForce (new Vector2 (Random.Range (-200,200), Random.Range (50,200)));
+	}
+
+	void OnCollisionEnter2d(Collider2D col){
+		if (col.gameObject.tag == "Player") {
+			Destroy(gameObject);
+		}
+	}
+}
